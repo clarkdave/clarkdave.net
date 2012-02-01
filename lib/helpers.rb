ae82601d02752ad2e3500359958ef45435a86c1b
Dave Clark <me@clarkdave.net>
2012-02-01 21:57:30 +0000

    bunch more changes

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -33,7 +33,7 @@ module PostHelper
 		content = post.compiled_content
 		if content =~ /\s<!-- more -->\s/
 			content = content.partition('<!-- more -->').first +
-			"<div class='read-more'><a href='#{post.path}'>Read more</a></div>"
+			"<div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
 		end
 		return content
 	end

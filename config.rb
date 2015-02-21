c9e8b60a538a46eaaace148211399f2f380e51b0
Dave Clark <me@clarkdave.net>
2015-02-21 15:24:06 +0000

    moved assets into subdir

--- config.rb
+++ config.rb
@@ -32,40 +32,6 @@ require 'lib/blog_extensions'
 
 page '/feed.xml', :layout => false
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page '/path/to/file.html', layout: false
-#
-# With alternative layout
-# page '/path/to/file.html', layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page '/admin/*'
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
-#  which_fake_page: 'Rendering a fake page with a local variable' }
-
-###
-# Helpers
-###
-
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
@@ -73,9 +39,9 @@ activate :livereload
 activate :directory_indexes
 activate :syntax
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
 
 set :markdown_engine, :redcarpet
 set :markdown,
@@ -100,4 +66,12 @@ configure :build do
   #   options.threads = true
   #   options.image_extensions = %w(.png .jpg .gif .svg)
   # end
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :rsync
+  deploy.host = 'shell.clarkdave.net'
+  deploy.path = '/srv/clarkdave.net'
+  deploy.user = 'zoz'
 end
